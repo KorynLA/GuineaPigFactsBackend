1af7850aa802e1bbb1a19e532d086d0f8e4c2f67
Koryn Leslie-Arcaya <koryn.lesliearcaya@gmail.com>
2020-06-03 15:23:31 -0700

    Adding facts mapping
    Facts mapping will be used to get the facts from the backend.

--- guineaPigFacts/src/main/java/com/factsdemo/guineaPigFacts/GuineaPigFactsApplication.java
+++ guineaPigFacts/src/main/java/com/factsdemo/guineaPigFacts/GuineaPigFactsApplication.java
@@ -19,4 +19,9 @@ public class GuineaPigFactsApplication {
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
+	
+	@GetMapping('/facts')
+	public String facts() {
+		return String.format("facts");
+	}
 }
