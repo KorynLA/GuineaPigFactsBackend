3095087fabbf67c6b494ddf1edd20e932ddc89a8
KorynLA <leslieak@oregonstate.edu>
2020-06-03 11:58:03 -0700

    Using "Spring Quickstart Guide" to start building the application

---
+++ guineaPigFacts/src/main/java/com/factsdemo/guineaPigFacts/GuineaPigFactsApplication.java
@@ -0,0 +1,22 @@
+package com.factsdemo.guineaPigFacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GuineaPigFactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuineaPigFactsApplication.class, args);
+	}
+
+	//Web endpoint that has 1 parameter. When parameter isn't given, default value is world.
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
