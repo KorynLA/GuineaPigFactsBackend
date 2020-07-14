a1a1cca2f35ad5fdbcf20f77300725531fd24147
KorynLA <leslieak@oregonstate.edu>
2020-07-14 13:09:10 -0700

    Updating User model to provide id getter + Adding userServiceTests

--- guineaPigFacts/src/test/java/com/factsdemo/guineaPigFacts/serviceTests/UserServiceTest.java
+++ guineaPigFacts/src/test/java/com/factsdemo/guineaPigFacts/serviceTests/UserServiceTest.java
@@ -1,51 +1,125 @@
 package com.factsdemo.guineaPigFacts.serviceTests;
+
+import com.factsdemo.guineaPigFacts.models.Contact;
+import com.factsdemo.guineaPigFacts.models.User;
+import com.factsdemo.guineaPigFacts.repositories.UserRepository;
+import com.factsdemo.guineaPigFacts.services.UserService;
+import com.factsdemo.guineaPigFacts.services.implementations.UserServiceImplementation;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
 public class UserServiceTest {
-    /**
+
     @TestConfiguration
     static class FactServiceTestContextConfiguration {
         @Bean
         public UserService userService() {
-            return new UserService();
+            return new UserServiceImplementation();
         }
     }
     @Autowired
     UserService userService;
     @MockBean
     UserRepository userRepository;
+    private final String username = "testName";
+    private final String usernameTest1 = "test1";
+    private final String usernameTest2 = "test2";
+    private final String password = "testPassword";
+    private final String email = "testEmail@gmail.com";
+    private final boolean dailyUpdate = false;
+    private final String id = "100000";
+    private User user;
+    private Contact contactInfo;
 
+    /**
+     * Set up user and contact objects for testing
+     */
+    @Before
+    public void setUp() {
+        contactInfo = new Contact(email, dailyUpdate);
+        user = new User(username, password, contactInfo);
+        user.setId(id);
+    }
+
+    /**
+     * Test to verify user is saved once
+     */
     @Test
     public void userSaveOrUpdateTest() {
-        userService.saveOrUpdateFact(fact1);
-        Mockito.verify(userRepository, Mockito.times(1)).save(fact1);
-    }
-    @Test
-    public void userFindAllTest(){
-        Mockito.when(userRepository.findAll()).thenReturn(facts);
-        ArrayList<Fact> foundFacts = (ArrayList<Fact>) userService.findAll();
-        assertEquals(2, foundFacts.size());
+        userService.saveOrUpdateUser(user);
+        Mockito.verify(userRepository, Mockito.times(1)).save(user);
     }
 
+    /**
+     * Test to verify user is deleted once
+     */
     @Test
     public void userDeleteByIdTest() {
-        userService.delete(fact1.getId());
-        Mockito.verify(userRepository, Mockito.times(1)).deleteById(fact1.getId());
+        userService.deleteUser(user.getId());
+        Mockito.verify(userRepository, Mockito.times(1)).deleteById(user.getId());
     }
+
+    /**
+     * Test to verify user can be found by id
+     */
     @Test
     public void userFindByIdTest() {
-        Mockito.when(userRepository.findById(fact1.getId())).thenReturn(Optional.of(fact1));
-        Optional<Fact> found = userService.findById(idVal1);
+        Mockito.when(userRepository.findById(user.getId())).thenReturn(java.util.Optional.ofNullable(user));
+        Optional<User> found = userService.findById(id);
         assertTrue(found.isPresent());
     }
-    @Test
-    public void findByUsernameTest() {}
 
+    /**
+     * Test to verify user can be found by username
+     */
     @Test
-    public void findByEmailTest() {}
+    public void findByUsernameTest() {
+        Mockito.when(userRepository.findByUserName(user.getUserName())).thenReturn(user);
+        User found = userService.findByUserName(username);
+        assertNotNull(found);
+    }
 
+    /**
+     * Test to verify user can be found by email
+     */
     @Test
-    public void getCurrentUserTest() {}
+    public void findByEmailTest() {
+        Mockito.when(userRepository.findByContactInfo_Email(user.getContactInfo().getEmail())).thenReturn(user);
+        User found = userService.findByContact_Email(email);
+        assertNotNull(found);
+    }
 
+    /**
+     * Test to verify findByID is used when updating user
+     * Need to test save is called once
+     */
     @Test
-    public void updateUserTest() {}
-    **/
+    public void updateUserTest() {
+        user.setUserName(usernameTest1);
+        User newUser = userService.updateUser(id, user);
+        Mockito.verify(userRepository, Mockito.times(1)).findById(id);
+    }
+
+    /**
+     * Test to verify username has been updated for given id
+     */
+    @Test
+    public void updateUserTestUserNameChanged() {
+        user.setUserName(usernameTest2);
+        userService.updateUser(id, user);
+        Mockito.when(userRepository.findByUserName(user.getUserName())).thenReturn(user);
+        assertEquals(user.getUserName(), usernameTest2);
+    }
 }
