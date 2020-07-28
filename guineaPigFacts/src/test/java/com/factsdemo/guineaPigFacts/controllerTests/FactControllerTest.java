b090cc39487a9a7793aa84bc9c9ac1c27ae4a789
KorynLA <leslieak@oregonstate.edu>
2020-07-28 12:57:29 -0700

    Adding Fact controller GET tests

--- guineaPigFacts/src/test/java/com/factsdemo/guineaPigFacts/controllerTests/FactControllerTest.java
+++ guineaPigFacts/src/test/java/com/factsdemo/guineaPigFacts/controllerTests/FactControllerTest.java
@@ -1,16 +1,30 @@
 package com.factsdemo.guineaPigFacts.controllerTests;
 
 import com.factsdemo.guineaPigFacts.controllers.FactController;
+import com.factsdemo.guineaPigFacts.models.Fact;
 import com.factsdemo.guineaPigFacts.services.FactService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(FactController.class)
@@ -27,29 +41,65 @@ public class FactControllerTest {
     private final String goodId = "100000000000000000000000";
     private final String badId = "1";
     private final boolean approved = false;
+    private Fact fact;
+    List<Fact> factArray = new ArrayList<Fact>();
+    @Before
+    public void setup() {
+        Fact fact = new Fact(goodFactValue, approved);
+        fact.setId(goodId);
+        factArray.add(fact);
+    }
     /*
     * Get controller test '/fact/'
      */
     @Test
-    public void getBackslashControllerTest() {
+    public void getBackslashControllerTest() throws Exception {
+        Mockito.when(factService.findAll()).thenReturn(factArray);
+        mockMvc.perform(get("/fact/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].factValue", is(factArray.get(0).getFactValue())));
     }
     /*
      * Get controller test '/fact/home'
      */
     @Test
-    public void getHomeControllerTest() {
+    public void getHomeControllerTest() throws Exception {
+        Mockito.when(factService.findAll()).thenReturn(factArray);
+        mockMvc.perform(get("/fact/home")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].factValue", is(factArray.get(0).getFactValue())));
     }
     /*
      * Get controller test '/fact'
      */
     @Test
-    public void getControllerTest() {
+    public void getControllerTest() throws Exception {
+        Mockito.when(factService.findAll()).thenReturn(factArray);
+        mockMvc.perform(get("/fact")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].factValue", is(factArray.get(0).getFactValue())));
+    }
+    @Test
+    public void getEmptyControllerTest() throws Exception {
+        List<Fact> emptyFactList = new ArrayList<Fact>();
+        Mockito.when(factService.findAll()).thenReturn(emptyFactList);
+        mockMvc.perform(get("/fact")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
     }
     /*
      * Get controller test '/{id}' with good id
      */
     @Test
     public void getGoodIdControllerTest() {
+
     }
     /*
      * Get controller test '/{id}' with bad id
